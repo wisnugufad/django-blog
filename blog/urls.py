ec0a364553e4a4845ac6db40690007cc62e93ab8
Wisnu Gufa Destiawan <wisnugufadestiawan@gmail.com>
2020-11-27 10:41:20 +0700

    add readme img

--- blog/urls.py
+++ blog/urls.py
@@ -19,6 +19,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='home'),
-    path('about', views.about, name='about')
+    # path('', views.index, name='home'),
+    # path('about', views.about, name='about')
 ]
