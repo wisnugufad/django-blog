fab9f412e5d5064474a625e56d74bf025e3e8476
Wisnu Gufa Destiawan <wisnugufadestiawan@gmail.com>
2020-11-27 10:24:49 +0700

    add asset and templating, make route between home and about
    in this section adding the css and js module, configure static files in setting, and make a base template  an routing it, and also passing a value to view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,17 @@
+from django.http import HttpRequest
+from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-  return render(request, 'index.html')
+  data = {
+    'tittle':'Home',
+    'halaman':'Home'
+  }
+  return render(request, 'index.html', data)
+
+def about(request):
+  data = {
+    'tittle':'About',
+    'halaman':'About'
+  }
+  return render(request, 'about.html', data)
